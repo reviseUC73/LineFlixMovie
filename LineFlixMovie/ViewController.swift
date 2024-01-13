7603f45fedd42f6f5b0225bd8ba690134530c5cb
Setthanan Thongpanchang <87871212+reviseUC73@users.noreply.github.com>
2024-01-14 01:55:53 +0700

    Initial Commit

---
+++ LineFlixMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LineFlixMovie
+//
+//  Created by ReviseUC73 on 14/1/2567 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
