842552440946b85582879d1f109d1c8be2bee357
Setthanan Thongpanchang <87871212+reviseUC73@users.noreply.github.com>
2024-01-14 20:05:17 +0700

    add gitignore

---
+++ LineFlixMovie/Views/HeroHearderUIView.swift
@@ -0,0 +1,20 @@
+//
+//  HeroHearderUIView.swift
+//  LineFlixMovie
+//
+//  Created by ReviseUC73 on 14/1/2567 BE.
+//
+
+import UIKit
+
+class HeroHearderUIView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
