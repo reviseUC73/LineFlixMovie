1067c719fa6ddac30a68ab31c90cde6ee550b41f
Setthanan Thongpanchang <87871212+reviseUC73@users.noreply.github.com>
2024-01-15 01:28:54 +0700

    add ContentViewModel

---
+++ LineFlixMovie/ViewModels/ContentViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ContentViewModel.swift
+//  LineFlixMovie
+//
+//  Created by ReviseUC73 on 15/1/2567 BE.
+//
+
+import Foundation
+struct ContentViewModel {
+    let titleName: String
+    let posterURL: String
+}
